0349623b42b5498dc525399aef3f0f0ff3c50dcc
Ahmet FURTUN <ahmet.furtun1@gmail.com>
2021-02-21 16:51:03 +0300

    Started over with better file system

---
+++ src/components/app/app.component..tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+
+import '../../common/styles';
+import './app.component.css';
+import "nes.css/css/nes.min.css";
+
+const App:FC = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default App;
---
+++ src/pages/home/home.component.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+import './home.component.css';
+
+const Home:FC = () => {
+    return (
+        <div>
+            Home!
+        </div>
+    )
+}
+
+export default Home;
