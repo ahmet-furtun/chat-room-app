44aadaa01f5510eef2a560a85793965f0d1c62ce
cemilakkoc <delikanlix68@gmail.com>
2021-02-28 20:35:21 +0300

    updated the navbar

--- src/components/navbar-home/navbar-home.component.tsx
+++ src/components/navbar-home/navbar-home.component.tsx
@@ -1,9 +1,25 @@
-import React, { FC } from "react";
+import React, { FC, useState, useEffect } from "react";
 import {Link} from 'react-router-dom';
+
+import store from "../../redux/store";
+import { logoutUser } from "../../redux/actions/userActions"
 
 import './navbar-home.component.css';
 
 const HomeNavbar: FC = () => {
+  const [state, setState]: any = useState(store.getState())
+
+  useEffect(() => {
+    setTimeout(() => {
+      setState(store.getState());
+      console.log(state)
+    }, 0)
+  }, []);
+
+  let handleLogout = () => {
+    store.dispatch(logoutUser());
+  }
+  
   return (
     <nav>
       <input type="checkbox" id="check" />
@@ -15,12 +31,24 @@ const HomeNavbar: FC = () => {
         <li>
           <Link to="/">Home</Link>
         </li>
-        <li>
-          <Link to="/login">Log In</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
+        {
+          state.user.authenticated ? (
+            <>
+              <li>
+                <a href="#" onClick={handleLogout}>Logout</a>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/login">Log In</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+            </>
+          )
+        }
       </ul>
     </nav>
   );
